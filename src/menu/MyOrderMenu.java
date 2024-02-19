c9d59972755617b1c289276a14d4746d407a59a2
Darjusch Schrand <Darjusch.schrand@code.berlin>
2024-02-19 10:53:40 +0100

    case when no orders in history and history is viewed

--- src/menu/MyOrderMenu.java
+++ src/menu/MyOrderMenu.java
@@ -18,6 +18,11 @@ public class MyOrderMenu implements Menu {
             System.out.println("Please, log in or create new account to see list of your orders");
             return;
         }
+        if (context.getPurchaseHistory().size() > 0) {
+            System.out.println(
+                    "Unfortunately, you don't have any orders yet. Navigate back to main menu to place a new order");
+            return;
+        }
         printMenuHeader();
         System.out.println("This is your Purchase History: ");
         String productsString = context.getPurchaseHistory().stream()
